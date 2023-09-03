19add36c4a0e919e51ead5986dbdb1c038ee4f83
Sandun Rathsara <sandun@surge.global>
2023-09-04 00:28:54 +0530

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={`w-screen h-screen bg-gradient-to-r from-fuchsia-950 to-violet-950 flex flex-col gap-2 justify-center items-center`}>
+      <span className={`text-white text-5xl`}>This is my space</span>
+      <span className={`text-white`}>W A S R</span>
+    </div>
+  )
+}
