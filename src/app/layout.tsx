df2801af672d98ba655a3121ffbfaae58b514f71
Sandun Rathsara <srathsara1@gmail.com>
2024-02-17 16:03:21 +0530

    upgraded from pages directory to app directory

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sandun Rathsara",
+  description: "This is my space - WASR",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div
+      className={`w-screen h-screen bg-gradient-to-r from-fuchsia-950 to-violet-950 flex flex-col gap-2 justify-center items-center`}
+    >
+      <span className={`text-white text-5xl`}>This is my space</span>
+      <span className={`text-white`}>W A S R</span>
+    </div>
+  );
+}
